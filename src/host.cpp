256571a81af715afb3c66e1cae6d9b0f9c1ef85e
YsnBnc <ysnbnc05@gmail.com>
2026-06-27 13:32:12 +0300

    Upload src/host.cpp

--- src/host.cpp
+++ src/host.cpp
@@ -3,10 +3,8 @@
 #include <iostream>
 
 #ifdef _WIN32
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <winsock2.h>
 #include <ws2tcpip.h>
-#pragma comment(lib, "Ws2_32.lib") // Links the winsock library
 #else
 #include <netinet/in.h>
 #include <sys/socket.h>
@@ -17,38 +15,71 @@ using namespace std;
 
 int main()
 {
-    // creating socket
-    int hostSocket = socket(AF_INET, SOCK_STREAM, 0);
+    int hostSocket, newSocket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024];
 
-    // specifying the address
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
+#ifdef _WIN32
+    WSAData wsaData;
+    int wsaResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (wsaResult != 0) {
+        cerr << "WSAStartup() failed." << endl;
+        return 1;
+    }
+#endif
 
-    // binding socket.
-    bind(hostSocket, (struct sockaddr*)&serverAddress,
-         sizeof(serverAddress));
-
-    // listening to the assigned socket
-    listen(hostSocket, 5);
-
-    // accepting connection request
-    int clientSocket= accept(hostSocket, nullptr, nullptr);
-    // sending data
-    while (true)
-    {
-        char buffer[1024] = { 0 };
-        recv(clientSocket, buffer, sizeof(buffer), 0);
-        cout << "Message from client: " << buffer << endl;
+    //Create socket
+    hostSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (hostSocket == 0) {
+        cerr << "socket failed." << endl;
+        exit(EXIT_FAILURE);
     }
 
-    // closing socket
-    #ifdef _WIN32
-    closesocket(hostSocket);
-    #else
-    close(hostSocket);
-    #endif
+    //Assign socket to a port
+#ifdef _WIN32
+    setsockopt(hostSocket, SOL_SOCKET, SO_REUSEADDR, (const char *)&opt, sizeof(opt));
+#else
+    setsockopt(hostSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+#endif
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
 
+    //Bind
+    if (bind(hostSocket, (struct sockaddr *)&address, sizeof(address)) == 0) {
+        cerr << "bind failed." << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    //Listen
+    if (listen(hostSocket, 10) < 0) {
+        cerr << "listen failed." << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    //Accept connections
+    newSocket = accept(hostSocket, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+    if (newSocket < 0) {
+        cerr << "accept failed." << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    //Read data
+#ifdef _WIN32
+    recv(newSocket, buffer, sizeof(buffer), 0);
+#else
+    read(newSocket, buffer, sizeof(buffer), 0);
+#endif
+    cout << "Message from client: " << buffer << endl;
+
+#ifdef _WIN32
+    closesocket(newSocket);
+    closesocket(hostSocket);
+#else
+    close(newSocket);
+    close(hostSocket);
+#endif
     return 0;
 }
