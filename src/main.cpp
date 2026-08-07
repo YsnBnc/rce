499ec201cf185cfc2cf0294f16933e43e0bacc46
YsnBnc <ysnbnc05@gmail.com>
2026-08-07 11:55:49 +0300

    basic UI

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,97 @@
 #include <iostream>
-#include <limits>
-
+#include <wx/simplebook.h>
+#include <wx/wx.h>
 #include "utilities/bridge.h"
 using namespace std;
 
+int PORT;
+char TARGET_IP[16];
+std::string COMMAND;
+std::string PATH_TO_FILE;
+
+
+class RCE_App : public wxApp
+{
+public:
+    bool OnInit() override;
+};
+
+wxIMPLEMENT_APP(RCE_App);
+
+class RCE_Frame : public wxFrame
+{
+public:
+    enum
+    {
+        ID_SERVER_SIDE = 1001,
+        ID_CLIENT_SIDE = 1002,
+    };
+
+    RCE_Frame():wxFrame(nullptr, wxID_ANY, "Remote Code Execution", wxDefaultPosition)
+    {
+        wxSize fixedSize(800, 600); //Lock the window size
+        SetMinSize(fixedSize);
+        SetMaxSize(fixedSize);
+
+        m_book = new wxSimplebook(this, wxID_ANY); //Page container
+
+        //Permanent panels
+        initServerPanel();
+        initClientPanel();
+
+        wxMenu *menuView = new wxMenu();
+        menuView->Append(ID_SERVER_SIDE, wxString("&Server Side"));
+        menuView->Append(ID_CLIENT_SIDE, wxString("&Client Side"));
+
+        wxMenuBar *menu_bar = new wxMenuBar();
+        menu_bar->Append(menuView, wxString("&Sides"));
+        SetMenuBar(menu_bar);
+
+        CreateStatusBar();
+        SetStatusText("Ready");
+
+        Bind(wxEVT_MENU, &RCE_Frame::OnServerSide, this, ID_SERVER_SIDE);
+        Bind(wxEVT_MENU, &RCE_Frame::OnClientSide, this, ID_CLIENT_SIDE);
+    }
+
+private:
+    wxSimplebook* m_book = nullptr;
+    wxPanel* serverPanel = nullptr;
+    wxPanel* clientPanel = nullptr;
+
+    void onButtonClicked(wxCommandEvent& event) {
+        wxMessageBox("Hello world from wxWidgets!", "", wxOK | wxICON_INFORMATION);
+    }
+
+    void initServerPanel() {
+        serverPanel = new wxPanel(m_book);
+        new wxStaticText(serverPanel, wxID_ANY, "Server Side", wxPoint(100,70), wxSize(150,100));
+        m_book->AddPage(serverPanel,"Server Side", true);
+    }
+    void initClientPanel() {
+        clientPanel = new wxPanel(m_book);
+        new wxStaticText(clientPanel, wxID_ANY, "Client Side",wxPoint(100, 70));
+        m_book->AddPage(clientPanel,"Client Side", true);
+    }
+    void OnServerSide(wxCommandEvent& event) {
+        m_book->SetSelection(0);
+        SetStatusText("Server Side View");
+    }
+    void OnClientSide(wxCommandEvent& event) {
+        m_book->SetSelection(1);
+        SetStatusText("Client Side View");
+    }
+};
+
+bool RCE_App::OnInit()
+{
+    RCE_Frame *frame = new RCE_Frame();
+    frame->Show(true);
+    return true;
+}
+
+
+/*
 int PORT;
 char TARGET_IP[16];
 std::string COMMAND;
@@ -32,6 +120,7 @@ int server() {
 }
 
 int main() {
+
     int side = 0;
     cout << "Remote Code Execution\n" << "Specify a side\n 1. Server\n 2. Client\n";
     cin >> side;
@@ -46,5 +135,7 @@ int main() {
             cout << "Invalid command\n";
             return 1;
     }
-}
 
+
+}
+*/
